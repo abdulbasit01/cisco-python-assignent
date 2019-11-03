81dfd2a518968213c8c5de799d23229505695a7a
abdulbasit01 <abdulbasit14827@gmail.com>
2019-11-03 20:49:07 +0500

    assignment commit 1

---
+++ assignemnt.py
@@ -0,0 +1,26 @@
+#queation 1
+line_1="Twinkle, twinkle, little star,\n \t How I wonder what you are! \n \t \tUp above the world so high, \n \t \tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n \t How I wonder what you are."
+print(line_1)
+#queation 2
+import sys
+print (sys.version)
+#question 3
+from datetime import datetime
+now = datetime.now()
+print("Today's date:", now)
+curretH=now.time()
+print(curretH)
+#question 4
+import math
+radius=input("Enter raduis\n")
+area=2*(math.pi)*float(radius**2)
+print("the are of the circle is",area)
+# question 5
+firstName=str(input("Enter Your First Name\n"))
+lastName=str(input("Enter Your Last Name\n"))
+print(lastName +" "+firstName)
+#question 6
+int1=str(input("Enter Your First Name\n"))
+int2=str(input("Enter Your Last Name\n"))
+sum=int(int1)+int(int2)
+print("the sum of two numbers is ",sum)
